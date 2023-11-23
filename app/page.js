383f04ba586a0c183f68d41385eb1627cd6e4d37
muratcan <muratcankirdar@gmail.com>
2023-11-23 23:04:43 +0300

    change main height

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className='min-h-screen flex flex-col text-center items-center py-4'>
+    <main className='h-full flex flex-col text-center items-center py-4'>
       <div className='rounded-full bg-gray-50 w-24 h-24 animate-pulse' />
     </main>
   );
