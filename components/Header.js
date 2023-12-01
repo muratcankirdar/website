8309aea5b0570ad7e3962e2ac75c45934f1c74ae
muratcan <muratcankirdar@gmail.com>
2023-12-01 23:26:36 +0300

    Add LinkedIn and GitHub logos to the header

--- components/Header.js
+++ components/Header.js
@@ -1,22 +1,24 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { social_links } from '@/utils/constants';
 import classNames from 'classnames';
 
 export default function Header() {
   const { LINKEDIN, GITHUB } = social_links;
+
   return (
     <header className={classNames('flex flex-row justify-between p-4')}>
       <h2 className='text-xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600'>
         Murat Can Kırdar
       </h2>
 
-      <div className='flex flex-row gap-2'>
+      <div className='flex flex-row gap-2 items-center'>
         <Link href={LINKEDIN} target={'_blank'}>
-          LinkedIn
+          <Image src='/linkedin.png' alt='LinkedIn Logo' width={24} height={24} />
         </Link>
 
         <Link href={GITHUB} target={'_blank'}>
-          Github
+          <Image src='/github.svg' alt='GitHub Logo' width={24} height={24} />
         </Link>
       </div>
     </header>
