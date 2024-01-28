9592ec6fdaa39dc6e19824a0810bf2e1c9d0dc51
muratcan <muratcankirdar@gmail.com>
2024-01-28 16:35:18 +0300

    update design

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ interface Props {}
 
 const Header: FC<Props> = () => {
   return (
-    <header className="m-2 flex flex-row justify-between rounded-lg border p-2">
+    <header className="flex flex-row justify-between rounded-lg border px-6 py-2 md:py-3 m-2">
       <span className="font-bold">Murat Can</span>
 
       <div className="flex flex-row gap-2">
